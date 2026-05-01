8b6b23e8ba30c8ad6e17a62bc5ccbc87849f3c06
AntoineDrillaud <antoinedrillaud2207@icloud.com>
2026-05-01 13:39:59 +0200

    enemis plus grands

--- Codes/assets.py
+++ Codes/assets.py
@@ -17,13 +17,13 @@ def load_assets():
     assets["floor"]  = pygame.transform.scale(assets["floor"],  (TILE_SIZE, TILE_SIZE))
     assets["wall"]   = pygame.transform.scale(assets["wall"],   (TILE_SIZE, TILE_SIZE))
     assets["door"]   = pygame.transform.scale(assets["door"],   (TILE_SIZE, TILE_SIZE))
-    assets["enemy"]  = pygame.transform.scale(assets["enemy"],  (40, 40))
+    assets["enemy"]  = pygame.transform.scale(assets["enemy"],  (47, 47))
     assets["player"] = pygame.transform.scale(assets["player"], (40, 40))
 
     assets["heart"] = pygame.image.load(os.path.join(base, "assets", "heart.png")).convert_alpha()
     assets["speed"] = pygame.image.load(os.path.join(base, "assets", "speed.png")).convert_alpha()
 
-    assets["heart"] = pygame.transform.scale(assets["heart"], (28, 28))
-    assets["speed"] = pygame.transform.scale(assets["speed"], (28, 28))
+    assets["heart"] = pygame.transform.scale(assets["heart"], (25, 25))
+    assets["speed"] = pygame.transform.scale(assets["speed"], (25, 25))
 
     return assets
